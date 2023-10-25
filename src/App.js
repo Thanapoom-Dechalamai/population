3381c8b479009aa9f54b3a931301f270e573a668
Thanapoom-Dechalamai <fgv5386@gmail.com>
2023-10-25 13:09:32 +0700

    Revert "change lib"
    This reverts commit bf915c1bb3ad8afa66e572ea596f8c73edf33a2f.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import Papa from 'papaparse';
 import poppulationData from './data/population-and-demography.csv';
 import { Col, Container, Row } from 'react-bootstrap';
 import './App.css';
-import Chartrace from './component/Chartrace';
 
 const colorCache = {};
 function simpleHash(str)
@@ -98,130 +97,129 @@ const App = () =>
 
 
   return (
-    <Chartrace />
-    // <Container>
-    //   <Row>
-    //     <Col xs={12} className='mt-3' style={{ color: "#404040" }}>
-    //       <div className='fw-bold fs-2'>
-    //         Population growth per country, 1950 to 2021
-    //       </div>
-    //     </Col>
-    //     <Col xs={12} className='align-items-center d-flex'>
-    //       <div className='fw-bold fs-2' style={{ color: "#404040" }}>
-    //         Year: {currentYear}
-    //         <div className='fs-4'>
-    //           Total: {totalPop}
-    //         </div>
-    //       </div>
+    <Container>
+      <Row>
+        <Col xs={12} className='mt-3' style={{ color: "#404040" }}>
+          <div className='fw-bold fs-2'>
+            Population growth per country, 1950 to 2021
+          </div>
+        </Col>
+        <Col xs={12} className='align-items-center d-flex'>
+          <div className='fw-bold fs-2' style={{ color: "#404040" }}>
+            Year: {currentYear}
+            <div className='fs-4'>
+              Total: {totalPop}
+            </div>
+          </div>
 
-    //     </Col>
-    //     <Col>
-    //       <ResponsiveContainer width="100%" minHeight="65vh">
-    //         <ComposedChart
-    //           layout="vertical"
-    //           data={filteredData}
-    //           margin={{
-    //             top: 20,
-    //             right: 100,
-    //             bottom: 20,
-    //             left: 30,
-    //           }}
-    //         >
-    //           <XAxis
-    //             type='number'
-    //             domain={[0, dataMax]}
-    //             allowDataOverflow
-    //             tickCount={8}
-    //             tickFormatter={(value) => `${value.toLocaleString()}`}
-    //           />
-    //           <YAxis dataKey="Country name" type="category" />
-    //           <CartesianGrid stroke="#f5f5f5" />
-    //           <Bar dataKey="Population" fill="#8080ff">
-    //             <LabelList
-    //               fill="gray"
-    //               dataKey="Population"
-    //               position="right"
-    //               formatter={(value) =>
-    //               {
-    //                 value = parseInt(value);
-    //                 return value.toLocaleString();
-    //               }}
+        </Col>
+        <Col>
+          <ResponsiveContainer width="100%" minHeight="65vh">
+            <ComposedChart
+              layout="vertical"
+              data={filteredData}
+              margin={{
+                top: 20,
+                right: 100,
+                bottom: 20,
+                left: 30,
+              }}
+            >
+              <XAxis
+                type='number'
+                domain={[0, dataMax]}
+                allowDataOverflow
+                tickCount={8}
+                tickFormatter={(value) => `${value.toLocaleString()}`}
+              />
+              <YAxis dataKey="Country name" type="category" />
+              <CartesianGrid stroke="#f5f5f5" />
+              <Bar dataKey="Population" fill="#8080ff">
+                <LabelList
+                  fill="gray"
+                  dataKey="Population"
+                  position="right"
+                  formatter={(value) =>
+                  {
+                    value = parseInt(value);
+                    return value.toLocaleString();
+                  }}
 
-    //             />
-    //             {filteredData.map((entry, index) => (
-    //               <Cell key={`cell-${index}`} fill={getColor(entry["Country name"])} />
-    //             ))}
-    //           </Bar>
-    //           <Tooltip formatter={(value, name, props) => [parseInt(value).toLocaleString(), name]} />
-    //           <Legend />
-    //         </ComposedChart>
-    //       </ResponsiveContainer>
+                />
+                {filteredData.map((entry, index) => (
+                  <Cell key={`cell-${index}`} fill={getColor(entry["Country name"])} />
+                ))}
+              </Bar>
+              <Tooltip formatter={(value, name, props) => [parseInt(value).toLocaleString(), name]} />
+              <Legend />
+            </ComposedChart>
+          </ResponsiveContainer>
 
-    //     </Col>
+        </Col>
 
-    //     <div className='d-flex align-items-center'>
-    //       <button className='rounded timelapse-button' onClick={() =>
-    //       {
-    //         if (currentYear === 2021)
-    //         {
-    //           setCurrentYear(1950);
-    //         }
-    //         setIsPlaying(!isPlaying);
-    //       }}>
-    //         {isPlaying ?
-    //           <>
-    //             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pause-fill" viewBox="0 0 16 16">
-    //               <path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z" />
-    //             </svg>
-    //             <span className='ms-2'>
-    //               Pause time-lapse
-    //             </span>
-    //           </> :
-    //           <>
-    //             <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
-    //               <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
-    //             </svg>
-    //             <span className='ms-2'>
-    //               Play time-lapse
-    //             </span>
-    //           </>
-    //         }
-    //       </button>
-    //       <button className='rounded year-button ms-3 me-3' onClick={() =>
-    //       {
+        <div className='d-flex align-items-center'>
+          <button className='rounded timelapse-button' onClick={() =>
+          {
+            if (currentYear === 2021)
+            {
+              setCurrentYear(1950);
+            }
+            setIsPlaying(!isPlaying);
+          }}>
+            {isPlaying ?
+              <>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-pause-fill" viewBox="0 0 16 16">
+                  <path d="M5.5 3.5A1.5 1.5 0 0 1 7 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5zm5 0A1.5 1.5 0 0 1 12 5v6a1.5 1.5 0 0 1-3 0V5a1.5 1.5 0 0 1 1.5-1.5z" />
+                </svg>
+                <span className='ms-2'>
+                  Pause time-lapse
+                </span>
+              </> :
+              <>
+                <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-play-fill" viewBox="0 0 16 16">
+                  <path d="m11.596 8.697-6.363 3.692c-.54.313-1.233-.066-1.233-.697V4.308c0-.63.692-1.01 1.233-.696l6.363 3.692a.802.802 0 0 1 0 1.393z" />
+                </svg>
+                <span className='ms-2'>
+                  Play time-lapse
+                </span>
+              </>
+            }
+          </button>
+          <button className='rounded year-button ms-3 me-3' onClick={() =>
+          {
 
-    //         setCurrentYear(1950);
-    //         setIsPlaying(false);
-    //       }}>
-    //         1950
-    //       </button>
-    //       <input
-    //         type="range"
-    //         min="1950"
-    //         max="2021"
-    //         step="1"
-    //         value={currentYear}
-    //         onChange={(e) =>
-    //         {
-    //           setIsPlaying(false);
-    //           setCurrentYear(parseInt(e.target.value));
-    //         }}
-    //         title={currentYear}
-    //         style={{
-    //           width: "70%",
-    //         }}
-    //       />
-    //       <button className='rounded year-button ms-3' onClick={() =>
-    //       {
+            setCurrentYear(1950);
+            setIsPlaying(false);
+          }}>
+            1950
+          </button>
+          <input
+            type="range"
+            min="1950"
+            max="2021"
+            step="1"
+            value={currentYear}
+            onChange={(e) =>
+            {
+              setIsPlaying(false);
+              setCurrentYear(parseInt(e.target.value));
+            }}
+            title={currentYear}
+            style={{
+              width: "70%",
+            }}
+          />
+          <button className='rounded year-button ms-3' onClick={() =>
+          {
 
-    //         setCurrentYear(2021);
-    //         setIsPlaying(false);
-    //       }}>
-    //         2021
-    //       </button>
-    //     </div>
-    //   </Row>
-    // </Container>
+            setCurrentYear(2021);
+            setIsPlaying(false);
+          }}>
+            2021
+          </button>
+        </div>
+      </Row>
+    </Container>
   );
 };
 
